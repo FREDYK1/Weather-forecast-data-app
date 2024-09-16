ceca2c150860f7a008e584c6690222fe3817cc08
FREDYK1 <fkwekukankam@gmail.com>
2024-09-16 09:46:17 +0000

    Extract forecast and kind form API

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import plotly.express as px
+from backend import get_data
 
 
 st.header("Weather Forecast for the Next Days")
@@ -10,3 +12,10 @@ days = st.slider("Forecast Days", min_value=1, max_value=5)
 options = st.selectbox("Select date to view", ("Temperature", "Sky"))
 
 st.subheader(f"{options} for the next {days} days in {place}")
+
+x,  y = get_data(place, days, options)
+
+figure = px.line(x, y, labels={"x":"Date", "y":"Temperature"})
+
+st.plotly_chart(figure)
+
